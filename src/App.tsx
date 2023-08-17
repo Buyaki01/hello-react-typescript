13835db13eeac0c972f3c67e97c37c73f0c0db7b
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-17 07:48:04 +0300

    add Conter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Counter } from "./components/Counter"
 import Heading from "./components/Heading"
 import { Section } from "./components/Section"
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <Heading title={"Hello"} />
       <Section title={"Different Title"}>This is my section</Section>
+      <Counter />
     </>
   )
 }
