d48a2c1b34ffeb06061fac05b8b5c5bb37419c86
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-17 07:18:36 +0300

    Add Section in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Heading from "./components/Heading"
+import { Section } from "./components/Section"
 
 function App() {
   return (
-    <Heading title={"Hello"} />
+    <>
+      <Heading title={"Hello"} />
+      <Section title={"Different Title"}>This is my section</Section>
+    </>
   )
 }
 
