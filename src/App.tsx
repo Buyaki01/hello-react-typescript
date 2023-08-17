083375a02058906f5a3fae8e1d872b0a61bd0e0d
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-17 11:53:50 +0300

    Add useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { useState, useEffect, useCallback, MouseEvent, KeyboardEvent } from "react"
+import { useState, useEffect, useCallback, useMemo, MouseEvent, KeyboardEvent } from "react"
 
 interface User {
   id: number,
   username: string,
 }
+
+type fibFunc = (n: number) => number
+
+const fib: fibFunc = (n) => {
+  if (n < 2) return n
+  return fib(n - 1) + fib(n - 2) 
+}
+
+const myNum: number = 37
 
 function App() {
   // const [count, setCount] = useState<User[]>([])
@@ -21,10 +30,13 @@ function App() {
 
   const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>) => setCount(prev => prev + 2), [])
 
+  const result = useMemo(() => fib(myNum), [myNum])
+
   return (
     <div className="App">
       <h1>{count}</h1>
       <button onClick={addTwo}>Add</button>
+      <h2>{result}</h2>
     </div>
   )
 }
