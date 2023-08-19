551780d67e237dd0240172694705c37b15c66606
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-19 08:00:20 +0300

    Add hooks in the Counter component

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,20 +1,23 @@
-import { ChangeEvent, ReactNode, useReducer } from 'react'
+import { ReactNode } from 'react'
+import { useCounter, useCounterText } from './context/CounterContext'
 
 type ChildrenType = {
   children: (num: number) => ReactNode
 }
 
-export const Counter = ({ children }: ChildrenType) => {
+export const Counter = ({ children }: ChildrenType) => { 
+  const { count, increment, decrement } = useCounter()
+  const { text, handleTextInput } = useCounterText()
+
   return (
     <>
-      {/* <h1>{children(count)}</h1> */}
-      <h1>{children(state.count)}</h1>
+      <h1>{children(count)}</h1>
       <div>
         <button onClick={increment}>+</button>
         <button onClick={decrement}>-</button>
       </div>
       <input type="text" onChange={handleTextInput} />
-      <h2>{state.text}</h2>
+      <h2>{text}</h2>
     </>
   )
 }
